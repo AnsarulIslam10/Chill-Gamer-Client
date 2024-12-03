e0a2c18638f41b5857763dbf350d97ef4d476296
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-04 00:42:55 +0600

    firebase and auth provider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthProvider from "./providers/AuthProvider";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </AuthProvider>
   </StrictMode>
 );
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/firebase.init';
+import { toast } from 'react-toastify';
+export const AuthContext = createContext(null)
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = () => {
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const signInUser = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const signInWithGoogle = () =>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    const signOutUser = () =>{
+        setLoading(true)
+        toast.success("Log-Out successful")
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () =>{
+            unSubscribe()
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createUser,
+        signInUser,
+        signInWithGoogle,
+        signOutUser,
+    }
     return (
-        <div>
+        <AuthContext.Provider value={authInfo}>
             
-        </div>
+        </AuthContext.Provider>
     );
 };
 
