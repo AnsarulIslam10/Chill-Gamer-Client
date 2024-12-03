9f31b22f16afd111ecb7fd370866538f97dc5be0
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-03 21:32:30 +0600

    navbar design done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import { FaBars } from "react-icons/fa6";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  const links = (
+    <>
+      <li><NavLink to={'/'}>Home</NavLink></li>
+      <li><NavLink to={'/allReviews'}>All Reviews</NavLink></li>
+      <li><NavLink to={'/addReview'}>ADD Review</NavLink></li>
+      <li><NavLink to={'/myReview'}>My Review</NavLink></li>
+      <li><NavLink to={'/gameWishlist'}>Game WishList</NavLink></li>
+    </>
+  );
+  return (
+    <nav className="max-w-7xl px-2 mx-auto">
+      <div className="navbar bg-base-100 px-0">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="mr-4 text-xl lg:hidden">
+              <FaBars/>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+             {links}
+            </ul>
+          </div>
+          <a className="font-bold px-0 text-purple-600 text-2xl">Chill Gamer</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal gap-2 px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link to={'/login'} className="btn bg-purple-500 rounded-none text-white">Login</Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
