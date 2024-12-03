72a9807b2f458c0d50b128f8e9cdcf22733bee55
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-03 21:40:31 +0600

    footer design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { FaBars } from "react-icons/fa6";
+import { FaBars, FaMoon, FaSun } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const links = (
     <>
-      <li><NavLink to={'/'}>Home</NavLink></li>
-      <li><NavLink to={'/allReviews'}>All Reviews</NavLink></li>
-      <li><NavLink to={'/addReview'}>ADD Review</NavLink></li>
-      <li><NavLink to={'/myReview'}>My Review</NavLink></li>
-      <li><NavLink to={'/gameWishlist'}>Game WishList</NavLink></li>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/allReviews"}>All Reviews</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/addReview"}>ADD Review</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/myReview"}>My Review</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/gameWishlist"}>Game WishList</NavLink>
+      </li>
     </>
   );
   return (
@@ -18,13 +28,13 @@ const Navbar = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="mr-4 text-xl lg:hidden">
-              <FaBars/>
+              <FaBars />
             </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-             {links}
+              {links}
             </ul>
           </div>
           <a className="font-bold px-0 text-purple-600 text-2xl">Chill Gamer</a>
@@ -33,7 +43,13 @@ const Navbar = () => {
           <ul className="menu menu-horizontal gap-2 px-1">{links}</ul>
         </div>
         <div className="navbar-end">
-          <Link to={'/login'} className="btn bg-purple-500 rounded-none text-white">Login</Link>
+         <button className="btn rounded-full mr-2"><FaMoon/></button>
+          <Link
+            to={"/login"}
+            className="btn bg-purple-500 rounded-none text-white"
+          >
+            Login
+          </Link>
         </div>
       </div>
     </nav>
