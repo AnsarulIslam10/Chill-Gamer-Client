82b2127b90df7bc6520b9d0928fe24575157b427
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-04 18:33:33 +0600

    delete my review

--- src/pages/MyReviews.jsx
+++ src/pages/MyReviews.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import { FaPen, FaTrash, FaX } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const MyReviews = () => {
   const { user } = useContext(AuthContext);
@@ -15,10 +16,40 @@ const MyReviews = () => {
       .catch((err) => console.log(err));
   }, [user]);
 
-    if (!myReviews || myReviews.length === 0) {
-      return <p>No Reviews found.</p>;
-    }
-  console.log(myReviews);
+  if (!myReviews || myReviews.length === 0) {
+    return <p>No Reviews found.</p>;
+  }
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/review/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your review has been deleted.",
+                icon: "success",
+              });
+              const remainingReviews = myReviews.filter((reviews) => reviews._id !== id);
+              setMyReviews(remainingReviews);
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="max-w-7xl mx-auto px-2 my-32">
       <div className="overflow-x-auto">
@@ -42,12 +73,12 @@ const MyReviews = () => {
                 <td>{review.name}</td>
                 <td>{review.genres}</td>
                 <td className="text-center">
-                    <button className="btn btn-circle mr-2 bg-purple-500 text-white">
-                        <FaPen/>
-                    </button>
-                    <button className="btn btn-circle bg-red-500 text-white">
-                        <FaTrash/>
-                    </button>
+                  <button className="btn btn-circle mr-2 bg-purple-500 text-white">
+                    <FaPen />
+                  </button>
+                  <button onClick={()=>handleDelete(review._id)} className="btn btn-circle bg-red-500 text-white">
+                    <FaTrash />
+                  </button>
                 </td>
               </tr>
             ))}
