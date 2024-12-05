1901d66e5c3c0b24f02887776fa7f6064d3b6cfe
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-05 23:35:07 +0600

    react typewriter npm package used

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 const Banner = () => {
+  const [text1] = useTypewriter({
+    words: ['Latest', 'Game', 'Reviews'],
+    loop: true
+  })
   return (
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
-        {/* information */}
-          <div className="absolute text-neutral-100 p-6">
-            <h2>Red Dead Redemption 2</h2>
-            <p>An epic story of survival and betrayal in the wild, wild west</p>
-          </div>
         <img
           src="https://i.ibb.co.com/X8dT1V6/rdr2.jpg"
           className="w-full"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle btn-sm sm:btn-md">
+          <a href="#slide3" className="btn btn-circle btn-sm sm:btn-md">
             ❮
           </a>
           <a href="#slide2" className="btn btn-circle btn-sm sm:btn-md">
             ❯
           </a>
+        </div>
+        <div className="absolute bottom-10 left-5 text-white max-w-sm">
+          <span className="text-3xl font-semibold">{text1}</span>
+          <Cursor cursorColor='red' />
+          <p className="text-gray-300">Discover top games with in-depth reviews, honest ratings, and expert opinions to guide your next adventure!.</p>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
