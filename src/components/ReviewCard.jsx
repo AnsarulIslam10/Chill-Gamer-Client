fa774cb4bf1a17fe6b91026df28ad692760b58ad
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-05 10:30:26 +0600

    challenge part, sort all reviews

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from 'react-router-dom';
-const ReviewCard = ({review}) => {
-    const {_id, name, cover, rating, genres} = review
-    return (
-        <div className='flex flex-col justify-center items-center border p-6 hover:scale-105 transition-all duration-300'>
-            <img className='mb-3' src={cover} alt="" />
-            <h2 className='text-2xl font-semibold'>{name}</h2>
-            <div className="flex items-center">
-            <ReactStars
-              count={5}
-              value={rating}
-              size={24}
-              activeColor="#ffd700"
-            />
-            <p>({rating})</p>
-          </div>
-            <h1>{genres}</h1>
+import { Link } from "react-router-dom";
+const ReviewCard = ({ review }) => {
+  const { _id, name, cover, rating, year, genres } = review;
+  return (
+    <div className="flex flex-col justify-center items-center border p-6 hover:scale-105 transition-all duration-300">
+      <img className="mb-3" src={cover} alt="" />
+      <h2 className="text-2xl font-semibold">{name}</h2>
+      <div className="flex items-center">
+        <ReactStars count={5} value={rating} size={24} activeColor="#ffd700" />
+        <p>({rating})</p>
+      </div>
+      <p>{year}</p>
+      <h1>{genres}</h1>
 
-            <Link to={`/review/${_id}`} className='btn bg-purple-500 rounded-none text-white mt-3'>Explore Details</Link>
-        </div>
-    );
+      <Link
+        to={`/review/${_id}`}
+        className="btn bg-purple-500 rounded-none text-white mt-3"
+      >
+        Explore Details
+      </Link>
+    </div>
+  );
 };
 
-export default ReviewCard;<h1>Hello</h1>
+export default ReviewCard;
+<h1>Hello</h1>;
--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import ReviewCard from "../components/ReviewCard";
+import { FaSort } from "react-icons/fa6";
 
 const AllReviews = () => {
   const loadedReviews = useLoaderData();
   const [reviews, setReviews] = useState(loadedReviews);
+
+  const handleSort = (e) => {
+    const sort = e.target.value;
+    let sortedReviews;
+    if (sort === "rating-ascending") {
+      sortedReviews = [...reviews].sort((a, b) => a.rating - b.rating);
+    } else if (sort === "rating-descending") {
+      sortedReviews = [...reviews].sort((a, b) => b.rating - a.rating);
+    } else if (sort === "year-ascending") {
+      sortedReviews = [...reviews].sort((a, b) => a.year - b.year);
+    } else if (sort === "year-descending") {
+      sortedReviews = [...reviews].sort((a, b) => b.year - a.year);
+    }
+    setReviews(sortedReviews);
+  };
   console.log(reviews);
   return (
     <div className="max-w-7xl px-2 mx-auto">
@@ -15,11 +31,32 @@ const AllReviews = () => {
           inventore error
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {reviews.map((review) => (
-            <ReviewCard key={review._id} review={review}></ReviewCard>
-          ))}
+      <div className="flex justify-end mr-10 mb-3">
+        <div>
+          <label className="label">
+            <span className="label-text text-white text-lg">Sort</span>
+          </label>
+          <select
+            className="select select-bordered rounded-none w-full"
+            name="ratings"
+            id=""
+            onChange={handleSort}
+          >
+            <option disabled selected>
+              Sort
+            </option>
+            <option value="rating-ascending">Low to High (Rating)</option>
+            <option value="rating-descending">High to Low (Rating)</option>
+            <option value="year-ascending">Oldest (Year)</option>
+            <option value="year-descending">Newest (Year)</option>
+          </select>
         </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {reviews.map((review) => (
+          <ReviewCard key={review._id} review={review}></ReviewCard>
+        ))}
+      </div>
     </div>
   );
 };
